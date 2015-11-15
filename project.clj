7fb18a782da1e096acaf6adf1501820114ef82f5
Dave Paola <dpaola2@gmail.com>
2015-11-14 18:21:05 -0800

    given a URL, find a list of URLs that contain .gif

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [
+                 [enlive "1.1.6"]
+                 [org.clojure/clojure "1.7.0"]
+                 ]
   :main ^:skip-aot gifdb.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/gifdb/core.clj
+++ src/gifdb/core.clj
@@ -1,5 +1,24 @@
 (ns gifdb.core
+  (:require [net.cgrand.enlive-html :as html])
   (:gen-class))
+
+(defn fetch-page [url]
+  (html/html-resource (java.net.URL. url)))
+
+(defn extract-sources [image-list]
+  ((image-list :attrs) :src)
+  )
+
+(defn fetch-images [url]
+  (map
+   extract-sources
+   (html/select (fetch-page url) [:img])))
+
+(defn fetch-gifs [url]
+  (remove
+   (fn [x] (not (.contains x ".gif")))
+   (fetch-images url))
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
